5c9f88ad6c36efb6199c5a9486b39a0ed3dd721f
Pham Nguyen Anh Khoi <phamnguyenanhkhoi21082005@gmail.com>
2026-05-14 14:02:19 +0700

    Fix error to build apk

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -84,4 +84,13 @@ dependencies {
 
     // Glide
     implementation("com.github.bumptech.glide:glide:4.16.0")
+
+    // Testing
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+    androidTestImplementation(platform(libs.androidx.compose.bom))
+    androidTestImplementation(libs.androidx.compose.ui.test.junit4)
+    debugImplementation(libs.androidx.compose.ui.tooling)
+    debugImplementation(libs.androidx.compose.ui.test.manifest)
 }
